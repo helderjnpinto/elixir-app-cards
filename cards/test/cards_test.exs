fff0aafee7dea2ff2f3a8d04e3edc299eaa6d973
Helder <helder@yarilabs.com>
2017-12-01 16:24:41 +0000

    update tests

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -5,4 +5,17 @@ defmodule CardsTest do
   test "the thruth" do
     assert 1 + 1 == 2
   end
+
+  test "Create_deck length 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+
+  test "Shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
+
+
 end
